40aaf1c4ae31a974b22f4396abaf439ff9683216
Meru <meruelodaniel@gmail.com>
2023-03-09 12:50:11 +0100

    Added spring initializr files.

---
+++ src/main/java/dmm/tfg/physicsengine/PhysicsEngineApplication.java
@@ -0,0 +1,13 @@
+package dmm.tfg.physicsengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhysicsEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhysicsEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dmm/tfg/physicsengine/PhysicsEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package dmm.tfg.physicsengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhysicsEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
